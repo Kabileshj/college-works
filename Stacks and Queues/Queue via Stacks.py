bba6809580bbdc7e42f58ad616839c1e7c360399
Kabileshj <61074401+Kabileshj@users.noreply.github.com>
2020-02-27 09:44:55 +0530

    Add files via upload

---
+++ Stacks and Queues/Queue via Stacks.py
@@ -0,0 +1,24 @@
+class Queue:
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def enqueue(self, data):
+        self.stack1.append(data)
+
+    def dequeue(self):
+        if len(self.stack2) == 0:
+            if (len(self.stack1) == 0):
+                return 'Cannot dequeue because queue is empty'
+        while (len(self.stack1) > 0):
+            p = self.stack1.pop()
+            self.stack2.append(p)
+        return self.stack2.pop()
+
+#Driver code
+q = Queue()
+array = [int(i) for i in input().split()]
+for value in array:
+    q.enqueue(value)
+for i in range(len(array)):
+    print(q.dequeue())
